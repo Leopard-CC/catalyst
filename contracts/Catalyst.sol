4b29ea80b0a9c47727cf67dbefe96400c443ab63
tikhan1019 <tikhanlesandovski@gmail.com>
2022-02-27 23:24:33 +0400

    :fire: remove unused improts

--- contracts/Catalyst.sol
+++ contracts/Catalyst.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "./Project.sol";
 import "./token/ERC20.sol";
 import "./access/Ownable.sol";
 import "./utils/IterableMapping.sol";
