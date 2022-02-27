abe7f5b80b05301746a67305812e1a2d99a251b7
tikhan1019 <tikhanlesandovski@gmail.com>
2022-02-27 23:30:09 +0400

    :rocket: add Incubation

---
+++ contracts/Incubation.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.12;
+
+import "./token/ERC20.sol";
+import "./access/Ownable.sol";
+
+contract Incubation is ERC20, Ownable {
+    constructor() ERC20("Payment Token", "PAY") {}
+
+    mapping(address => bool) private _isExpert;
+
+    function whitelistExpert(address _expertAddress, bool _status)
+        external
+        onlyOwner
+    {
+        _isExpert[_expertAddress] = _status;
+    }
+
+    function sendToProposer(address _proposer, uint _amount)
+        external
+        onlyOwner
+    {
+        _mint(_proposer, _amount);
+    }
+
+    function isExpert(address _address) external view returns (bool) {
+        return _isExpert[_address];
+    }
+
+    function transfer(address _address, uint _amount)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        require(_isExpert[_address], "Not a valid expert");
+        return super.transfer(_address, _amount);
+    }
+}
