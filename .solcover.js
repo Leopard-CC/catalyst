2523b1ef6db568fbafa43f99bc65fecaf30ac3e4
tikhan1019 <tikhanlesandovski@gmail.com>
2022-02-27 23:22:36 +0400

    :rotating_light: fix lint

--- .solcover.js
+++ .solcover.js
@@ -1,8 +1,8 @@
 module.exports = {
-    skipFiles: [
-        'access/Ownable.sol',
-        'token/ERC20.sol',
-        'utils/Context.sol',
-        'utils/IterableMapping.sol'
-    ]
+  skipFiles: [
+    "access/Ownable.sol",
+    "token/ERC20.sol",
+    "utils/Context.sol",
+    "utils/IterableMapping.sol",
+  ],
 };
